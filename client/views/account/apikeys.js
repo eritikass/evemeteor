d0663b0f9e407046afd954b18a0efae1463d0ac8
eritikass <eritikass@gmail.com>
2016-02-24 16:35:36 +0200

    show api keys

--- client/views/account/apikeys.js
+++ client/views/account/apikeys.js
@@ -1,3 +1,32 @@
 /**
  * Created by eritikass on 24/02/16.
  */
+
+Template.apikeys.helpers({
+    'getApikeys': function() {
+        Meteor.subscribe('user_apikeys')
+        return Apikeys.find({}, {sort: {created: -1}});
+    },
+    'stateGymp': function(state, type) {
+        switch (state) {
+            case 'new':
+                return 'minus';
+                break;
+
+            default:
+                return 'question-sign';
+        }
+    },
+    'keyinfo': function(row) {
+        if (row.info) {
+            return row.info;
+        }
+        if (row.state == 'new') {
+            return 'waiting for processing...';
+        }
+        if (row.state == 'process') {
+            return 'process';
+        }
+        return 'unknown, state=' + row.state;
+    }
+});
